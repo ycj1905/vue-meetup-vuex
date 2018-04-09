4cf30d4e92411e8aa9b0e5bd4757ab391e760790
Matt Jhou <ycj1665@gmail.com>
2018-04-09 17:51:37 +0800

    modulize the state management

---
+++ src/store/modules/counter.js
@@ -0,0 +1,48 @@
+const state = {
+    counter: 0
+};
+
+const getters = {
+    doubleCounter: state => {
+        return state.counter * 2;
+    },
+    stringCounter: state => {
+        return state.counter + ' Clicks';
+    },
+};
+
+const mutations = {
+    increment: (state, payload) => {
+        state.counter += payload;
+    },
+    decrement: (state, payload) => {
+        state.counter -= payload;
+    },
+};
+
+const actions = {
+    increment: (context, payload) => {
+        context.commit('increment', payload);
+    },
+    decrement: ({ commit }, payload) => {
+        commit('decrement', payload);
+    },
+    asyncIncrement: ({ commit }, payload) => {
+        console.log('asyncIncrement');
+        setTimeout(() => {
+            commit('increment', payload.by);
+        },payload.duration);
+    },
+    asyncDecrement: ({ commit }, payload) => {
+        setTimeout(() => {
+            commit('decrement', payload.by);
+        },payload.duration);
+    }
+}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
