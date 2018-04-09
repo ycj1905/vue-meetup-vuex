a9d6cd1c9c86307d99bb3a37bfbab86438d47249
Matt Jhou <ycj1665@gmail.com>
2018-04-09 16:30:04 +0800

    action

--- src/store/store.js
+++ src/store/store.js
@@ -22,5 +22,27 @@ export const store = new Vuex.Store({
         decrement: state => {
             state.counter --;
         }
+    },
+    actions: {
+        increment: context => {
+            context.commit('increment');
+        },
+        // ES6
+        // increment: ({ commit }) => {
+        //     commit('increment');
+        // }
+        decrement: ({ commit }) => {
+            commit('decrement');
+        },
+        asyncIncrement: ({ commit }) => {
+            setTimeout(() => {
+                commit('increment');
+            },2000);
+        },
+        asyncDecrement: ({ commit }) => {
+            setTimeout(() => {
+                commit('decrement');
+            },2000);
+        }
     }
 });
